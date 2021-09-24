65075fd7251c0c057f541fd64a9d195948dd4c4f
Leigh Bitossi <66171363+LIQBit@users.noreply.github.com>
2021-09-24 12:59:18 +0900

    first commit

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//password@420
+//
+
+mongoose
+    .connect(
+        'mongodb+srv://Leigh:test123@cluster0.cqb2q.mongodb.net',
+        { dbName: 'test', useNewUrlParser: true, useUnifiedTopology: true  })
+        .then(()=> {
+            console.log("connected to database");
+        });
+
+
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('listening for requests on port 4000');
+});
