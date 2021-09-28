4bd553c08385784282f6e402894d30b6f9d52c0d
Leigh Bitossi <66171363+LIQBit@users.noreply.github.com>
2021-09-28 13:46:17 +0900

    update: added BookList.js + GraphQlL functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React, {Component} from "react";
 import BookList from './Components/BookList';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+//apollo client setup
+const client = new ApolloClient({
+  uri:'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
 
 class App extends Component {
   render(){
       return (
-        <div id="main">
-          <h1>Reading List</h1>
-          <BookList/>
-        </div>
+        <ApolloProvider client={client}>
+          <div id="main">
+            <h1>Reading List</h1>
+            <BookList/>
+          </div>
+        </ApolloProvider>
       );
   }
 }
